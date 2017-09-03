123ba58e849c9baa7d6fda86eaccdf401063f5d6
wanjochan <wanjochan@gmail.com>
2017-09-03 13:54:17 +0800

    WJC: cleanup for 0.2

--- nodenodenode.js
+++ nodenodenode.js
@@ -6,49 +6,41 @@
  *         ws_host/ws_port        => web-socket server
  *                   + ws_secure  => wss
  */
-const Q = require('q');
 const util = require('util');
 var logger=console;//default logger
 function isEmpty(o,i){for(i in o){return!1}return!0}
-function copy_o2o(o1,o2){ for(var k in o2){ o1[k]=o2[k]; } return o1; }//copy from o2 to o1, but notes that didn't check o1 here...
-function argv2o(argv,m,mm){ var rt={};for(k in argv)(m=(rt[""+k]=argv[k]).match(/^(\/|--?)([a-zA-Z0-9-_]*)=(.*)/))&&(rt[m[2]]=(mm=m[3].match(/^".*"$/))?mm[1]:m[3]);return rt; }
+function copy_o2o(o1,o2){for(var k in o2){o1[k]=o2[k]}return o1}
+function argv2o(argv,m,mm){var rt={};for(k in argv)(m=(rt[""+k]=argv[k]).match(/^(\/|--?)([a-zA-Z0-9-_]*)=(.*)/))&&(rt[m[2]]=(mm=m[3].match(/^".*"$/))?mm[1]:m[3]);return rt}
 var argo={};
-var flag_init_ok=false;
+var flag_daemon=false;
 module.exports=this_argo=>{
 	if(!isEmpty(this_argo)) copy_o2o(argo,this_argo);
 	var rt={STS:'OK'};
 	copy_o2o(argo,argv2o(process.argv));
 	if(typeof(nw)!='undefined'){
 		copy_o2o(argo,argv2o(nw.App.argv));
-		argo.is_nwjs=true;
-		//nwjs special...
+		rt.is_nwjs=true;
 		logger={log:function(){
 			try{console.log(util.format.apply(null, arguments))}catch(ex){
 				console.log.apply(console,arguments);}
 		}};
 	}
 	if(typeof(global)!='undefined'){
-		argo.has_global=true;
+		rt.has_global=has_global=true;
 	}
-	//logger.log('!!!!!!!!!!',argo);
 	for(var k in argo){
 		logger.log('argo.'+k+' is '+typeof(argo[k]));
 	}
-
 	process.env.UV_THREADPOOL_SIZE = argo.UV_THREADPOOL_SIZE || 126;//thread pool for uv_queue_work()
-
 	logger.log(process.env);
-
 	logger.log("process.versions=",process.versions);
-
 	if(!argo.app){
 		if(!argo.approot){//must specify the approot for default egapp
 			//throw new Error('-approot is needed if -app is absent');
-			argo.approot=process.cwd();
+			rt.approot=argo.approot=process.cwd();
 		}
 		argo.app=__dirname + '/egapp.js';//load the default egapp ...
 	}
-
 	var appModule=require(argo.app)({argo});
 
 	////////////////////////////////////////////////////////// HTTP => APP
@@ -56,11 +48,11 @@ module.exports=this_argo=>{
 	//var http_server=require('http').createServer(appModule({argo}));
 	if(http_port){
 		if(!appModule.handleHttp) throw new Exception('appModule.handleHttp is not defined.');
-		argo.http_server=require('http').createServer(appModule.handleHttp);//let the internal logic can access
+		rt.http_server=require('http').createServer(appModule.handleHttp);//let the internal logic can access
 		try{
-			argo.http_server.listen(http_port,http_host,()=>{logger.log('http listen on ',http_host,':',http_port)});
+			rt.http_server.listen(http_port,http_host,()=>{logger.log('http listen on ',http_host,':',http_port)});
 			rt.flag_http=true;
-			flag_init_ok=true;
+			flag_daemon=true;
 		}catch(ex){
 			logger.log('failed to start http_server on '+http_host+':'+http_port);
 			logger.log(''+ex);
@@ -77,10 +69,10 @@ module.exports=this_argo=>{
 			key: fs.readFileSync(https_key),
 			cert: fs.readFileSync(https_cert)
 		};
-		argo.https_server=require('https').createServer(appModule.handleHttps);//let the internal logic can access
+		rt.https_server=require('https').createServer(appModule.handleHttps);//let the internal logic can access
 		try{
-			argo.https_server.listen(https_host,https_host,()=>{logger.log('https listen on ',https_host,':',https_port)});
-			flag_init_ok=true;
+			rt.https_server.listen(https_host,https_host,()=>{logger.log('https listen on ',https_host,':',https_port)});
+			flag_daemon=true;
 			rt.flag_https=true;
 		}catch(ex){
 			logger.log('failed to start https_server on '+https_host+':'+https_port);
@@ -107,7 +99,7 @@ module.exports=this_argo=>{
 			ws.setMaxBufferLength(20971520);
 			var ws_opts={};
 			if (argo.ws_secure) ws_opts.secure=true;
-			var ws_server = argo.ws_server = ws.createServer(ws_opts);
+			var ws_server = rt.ws_server = ws.createServer(ws_opts);
 			ws_server.on('connection',function(conn){
 				var _addr=(conn.socket.remoteAddress);
 				var _port=(conn.socket.remotePort);
@@ -140,7 +132,7 @@ module.exports=this_argo=>{
 			logger.log("ws listen on "+ws_port);
 			try{
 				ws_server.listen(ws_port);
-				flag_init_ok=true;
+				flag_daemon=true;
 				rt.flag_ws=true;
 			}catch(ex){
 				logger.log('failed to start ws_server on '+ws_host+':'+ws_port);
@@ -150,31 +142,6 @@ module.exports=this_argo=>{
 			logger.log("ws.ex=",ex);
 		}
 	}
-	///////////////////////////////////// should be done inside appModule
-	//if(appModule.handleUncaughtException){
-	//	process.on('uncaughtException', err=>{
-	//		appModule.handleUncaughtException(err);
-	//	});
-	//}
-	//if(appModule.handleExit){
-	//	process.on("exit",function(x){
-	//		appModule.handleExit(x);
-	//	});
-	//}
-	//if(appModule.handleSIGINT){
-	//	process.on('SIGINT', function(){
-	//		appModule.handleSIGINT();
-	//	});
-	//}
-	//if(appModule.handleSIGTERM){
-	//	process.on('SIGTERM', function(){
-	//		appModule.handleSIGTERM();
-	//	});
-	//}
-	if(flag_init_ok){
-	}else{
-		//just warning, for maybe just some jobs done then exit...
-		rt.errmsg='WARNING: missing -ws_port or -server_port or -https_port';
-	}
-	return Q(rt);
+	rt.flag_daemon=flag_daemon;
+	return rt;
 };
