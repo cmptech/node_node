1491ce4a0380cfa71d867ce0280108ebb6723273
wanjochan <wanjochan@gmail.com>
2019-01-02 20:49:19 +0800

    npm

--- eg.sh
+++ eg.sh
@@ -14,4 +14,4 @@
 pm2 stop "test"
 pm2 delete "test"
 #pm2 start nodenodenode.js -i 2 --name "test" --watch "$PWD" -- /http_port=3333 /debug_level=2 /app=./egapp /logic=eglogic
-pm2 start nodenodenode.js -i 2 --name "test" --watch "$PWD" --ignore-watch "tmp logs" -- /http_port=3333 /app=./egapp /logic=eglogic
+pm2 start nodenodenode.js -i 0 --name "test" --watch "$PWD" --ignore-watch "tmp logs" -- /http_port=3333 /app=./egapp /logic=eglogic
